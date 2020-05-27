ccb99af6c07650389c4eb903bae950d9bcd19cf7
hexatester <revolusi147id@gmail.com>
2020-05-27 20:36:30 +0700

    del utils.conversation

--- klassbot/utils/conversation.py
+++
@@ -1,7 +0,0 @@
-from telegram.ext import ConversationHandler
-
-from klassbot.models import Conversation
-
-class CustomConversationHandler(ConversationHandler):
-    def _get_key(self, update):
-        pass
