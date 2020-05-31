ccff37a09560c0d53ab9cbcbf960b14139fa29a3
hexatester <revolusi147id@gmail.com>
2020-05-31 10:46:50 +0700

    improve initial commands

--- klassbot/handlers/command/start.py
+++ klassbot/handlers/command/start.py
@@ -1,11 +1,47 @@
-from telegram import Update
+from telegram import Update, Chat, ChatMember
 from telegram.ext import run_async, CallbackContext
 
-from klassbot.models import User
-from klassbot.utils.wrappers import message_wrapper
+from klassbot.models import Klass, User, UserKlass
+from klassbot.utils.wrappers import private_command_wrapper, klass_command_wrapper
 
 
 @run_async
-@message_wrapper(False)
+@private_command_wrapper()
 def start(update: Update, context: CallbackContext, user: User = None):
     update.effective_message.reply_text("Welcome!\nSend /help")
+
+
+@run_async
+@klass_command_wrapper(True)
+def start_klass(
+    update: Update,
+    context: CallbackContext,
+    user: UserKlass = None,
+    klass: Klass = None,
+):
+    if klass.started:
+        return
+    chat: Chat = update.effective_chat
+    chat_member: ChatMember = chat.get_member(user.user_id)
+    if not chat_member.status == ChatMember.CREATOR:
+        return
+    update.effective_message.reply_text(
+        """
+    Thanks, please give me permission for
+    Ban Users
+    Add Users
+    Pin Messages
+    Then send /create command.
+    """
+    )
+    if user:
+        user.from_chat(chat)
+    klass.creator = user.user
+    klass.started = True
+    return
+
+
+@run_async
+@private_command_wrapper()
+def start_join(update: Update, context: CallbackContext, user: User = None):
+    pass
